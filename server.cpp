e1b395c735ec7357719ae9cc3b6663bd6acafbab
Todd Johnston <johnstontoddc@gmail.com>
2023-05-12 23:10:56 -0600

    ADDING COMMANDS

--- server.cpp
+++ server.cpp
@@ -5,13 +5,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <poll.h>
+#include <sys/epoll.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include<vector>
 
+#include <iostream>
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -107,32 +108,43 @@ static void state_req(Conn *conn);
 static void state_res(Conn *conn);
 
 
-static bool try_one_request(Conn *conn){
+static bool try_one_request(Conn *conn) {
     if (conn->rbuf_size < 4) {
         return false;
     }
     uint32_t len = 0;
-    memcpy(&len,&conn->rbuf[0],4);
-    if(len > K_MAX_MSG){
+    memcpy(&len, &conn->rbuf[0], 4);
+    if (len > k_max_msg) {
         msg("too long");
         conn->state = STATE_END;
         return false;
     }
-    if(4 + len > conn->rbuf_size) {
+    if (4 + len > conn->rbuf_size) {
         return false;
     }
 
-    printf("client sats %.*s\n", len, &conn->rbuf[4]);
 
-    memcpy(&conn->wbuf[0], &len, 4);
-    memcpy(&conn->wbuf[4], &conn->rbuf[4],len);
-    conn->wbuf_size = 4+len;
+    uint32_t rescode = 0;
+    uint32_t wlen = 0;
+    int32_t do_request(
+        &conn->rbuf[4], len,
+        &rescode, &conn->wbuf[4+4],&wlen
+        );
 
+    if (err) {
+        conn->state = STATE_END;
+        return false;
+    }
+
+    wlen += 4;
+
+    memcpy(&conn->wbuf[0], &wlen, 4);
+    memcpy(&conn->wbuf[4], &rescode, 4);
+    conn->wbuf_size = 4 + wlen;
 
     size_t remain = conn->rbuf_size - 4 - len;
-
-    if(remain) {
-        memmove(conn->rbuf, &conn->rbuf[4+len], remain);
+    if (remain) {
+        memmove(conn->rbuf, &conn->rbuf[4 + len], remain);
     }
     conn->rbuf_size = remain;
 
@@ -146,8 +158,10 @@ static bool try_one_request(Conn *conn){
 static bool try_fill_buffer(Conn *conn) {
     assert(conn->rbuf_size < sizeof(conn->rbuf));
     ssize_t rv = 0;
+    memcpy()
     do{
         size_t cap = sizeof(conn->rbuf) - conn->rbuf_size;
+
         rv=read(conn->fd, &conn->rbuf[conn->rbuf_size], cap);
     }while(rv < 0 && errno == EINTR);
     if(rv < 0 && errno == EAGAIN) {
@@ -206,8 +220,107 @@ static bool try_flush_buffer(Conn*conn) {
     return true;
 }
 
+enum {
+    RES_OK = 0,
+    RES_ERR = 1,
+    RES_NX = 2
+};
+
+static std::map<std::string, std:string> g_map;
+
+static uint32_t do_get(
+    const std::vector<std::string> &cmd uint8_t *res, uint32_t *reslen)
+{
+    if(!g_map.count(cmd[1])) {
+        return RES_NX;
+    }
+    std::string &val = g_map[cmd[1]];
+    assert(val.size() <= K_MAX_MSG);
+    memcpy(res,val.data(), val.size());
+    *reslen = (uint32_t)val.size();
+
+    return RES_OK;
+}
+
+static uint32_t do_set(
+    const std::vector<std::string> & cmd, uint8_t *res, uint32_t *reslen)
+    {
+        (void)res;
+        (void)reslen;
+        g_map[cmd[1]] = cmd[2];
+        return RES_OK;
+    }
+
+static uint32_t do_del(
+    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    (void)res;
+    (void)reslen;
+    g_map.erase(cmd[1]);
+    return RES_OK;
+}
+const size_t K_MAX_ARGS = 1024;
+static int32_t parse_req(
+    const uint8_t *data, size_t len, std::vector<std::string> &out){
+
+    if(len < 4) {
+        return -1;
+    }
+     uint32_t n = 0;
+     memcpy(&n, *data[0], 4);
+     if(n > K_MAX_ARGS) {
+        return -1;
+     }
+
+     size_t pos = 4;
+     while(n--){
+        if (pos+4 > len) {
+            return -1;
+        }
+        uint32_t sz = 0;
+        memcpy(&sz, &data[pos],4);
+        if(pos + 4 + sz > len) {
+            return -1;
+        }
+        out.push_back(std::string((char *)&data[pos+4], sz));
+        pos += 4 + sz;
+     }
+     if(pos != len){
+        return -1;
+     }
+     return 0;
+}
+
+
 static void state_res(Conn *conn) {
     while(try_flush_buffer(conn)){}
+}
+
+static int32_t do_request(
+    const uint32_t *req, uint32_t reqlen,
+    uint32_t *rescode,uint8_t, uint32_t *reslen){
+
+    std::vector<std::string> cmd;
+    if(0 != parse_req(req, reqlen, cmd)) {
+        msg("bad req");
+        return -1;
+    }
+
+    if (cmd.size()  == 2 && cmd_is(cmd[0], "get")) {
+        *rescode = do_get(cmd,red,reslen);
+    }else if (cmd.size() == 3 && cmd_is(cmd[0],"set")) {
+        *rescode = do_set(cmd, res, reslen);
+    } else if (cmd.size() == 2 && cmd_is(cmd[0],"del")) {
+        *rescode = do_del(cmd, res, reslen);
+    } else {
+        *rescode = RES_ERR;
+        const char *msg = "Unknown cmd";
+        strcpy((char *)res, msg);
+        *reslen = strlen(msg);
+        return 0;
+    }
+    return 0;
+    
 }
 
 
@@ -252,33 +365,54 @@ int main()
     //A map of all client connections, keyed by fd
     std::vector<Conn *> fd2conn;
 
-    fd_set_nb(fd);
-
-    std::vector<struct pollfd> poll_args;
+    fd_set_nb(fd);  
+    std::vector<epoll_event> epoll_events;
 
     while(true){
-        poll_args.clear();
-        struct pollfd pfd = {fd,POLLIN,0};
-        poll_args.push_back(pfd);
+        
+        epoll_events.clear();
+
+        int epoll_fd = epoll_create(0);
+
+
+        if(epoll_fd){
+            die("epoll_createl()");
+        }
+
+        struct epoll_event efd;
+
+
+        efd.events = EPOLLIN;
+        efd.data.fd = fd;
+
+        int rv = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &efd);
+        if(rv<0){
+            die("epoll_ctl()");
+        }
+        epoll_events.push_back(efd);
         for(Conn* conn : fd2conn){
             if(!conn) {
                 continue;
             }
-            struct pollfd pfd = {};
-            pfd.fd = conn->fd;
-            pfd.events = (conn->state == STATE_REQ) ? POLLIN : POLLOUT;
-            pfd.events = pfd.events | POLLERR;
-            poll_args.push_back(pfd);
+            struct epoll_event efd = {};
+            efd.data.fd = conn->fd;
+            efd.events = (conn->state == STATE_REQ) ? EPOLLIN : EPOLLOUT;
+            efd.events = efd.events | EPOLLERR; 
+            int rv = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &efd);
+            if(rv<0){
+                die("epoll_ctl()");
+            }
+            epoll_events.push_back(efd);
         }
 
-        int rv = poll(poll_args.data(), (nfds_t)poll_args.size(),1000);
+        size_t event_count = epoll_wait(fd, epoll_events.data(),(int)epoll_events.size(),1000);
         if(rv < 0){
-            die("poll");
+            die("epoll_wait()");
         }
 
-        for(size_t i = 1; i < poll_args.size(); ++i){
-            if(poll_args[i].revents) {
-                Conn *conn = fd2conn[poll_args[i].fd];
+        for(size_t i = 1; i < event_count; ++i){
+            if(epoll_events[i].events) {
+                Conn *conn = fd2conn[epoll_events[i].data.fd];
                 connection_io(conn);
                 if(conn->state == STATE_END) {
                     fd2conn[conn->fd] = NULL;
@@ -287,7 +421,7 @@ int main()
                 }
             }
         }
-        if(poll_args[0].revents) {
+        if(epoll_events[0].events) {
             (void)accept_new_conn(fd2conn,fd);
         }
     }
